2d14d04acd58257c7952c2f16a9c47437170a247
warugaki <warugaki.web.developer@gmail.com>
2022-11-26 19:30:43 +0900

    グローバルスタイルの設定

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Global } from '@emotion/react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
-import '@/index.css';
+import GlobalStyle from '@/styles/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Global styles={GlobalStyle} />
     <App />
   </React.StrictMode>,
 );
